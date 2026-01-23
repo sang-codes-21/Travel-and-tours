37fc8fd7453515fd995980527223c1b4de2ae882
Sang codes <sungmoomoktan@gmail.com>
2026-01-23 16:29:36 +0545

    style:improved layout and readability

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -3,19 +3,19 @@ import Button from "../ui/Button";
 const About = () => {
   return (
     <section className="bg-gray-100">
-      <article className="mx-auto  max-w-7xl  flex  flex-col  lg:flex-row gap-8 py-16 ">
-        <div className="p-[4%]   lg:w-1/3">
+      <article className="mx-auto max-w-7xl flex flex-col lg:flex-row gap-8 py-16">
+        <div className="p-[4%] lg:w-1/3">
           <img
             src="https://images.unsplash.com/photo-1598751337485-0d57b0c50b83?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8dG91cmlzbXxlbnwwfHwwfHx8MA%3D%3D"
             alt="About Global Rising Tours & Travel"
-            className="h-[450px]  lg:w-full mx-auto  h-auto rounded-lg shadow-lg"
+            className="h-[450px] lg:w-full mx-auto h-auto rounded-lg shadow-lg"
           />
         </div>
         <div className="text-lg lg:pl-0 p-[4%] lg:w-2/3">
-          <h1 className=" text-lg text-[#004463]">
+          <h1 className="text-lg text-[#004463]">
             About Global Rising Tours & Travel
           </h1>
-          <h1 className="py-4 text-3xl font-semibold ">
+          <h1 className="py-4 text-3xl font-semibold">
             Travel That Fits Your Pace, Your Passion, Your Path
           </h1>
           <p>
@@ -37,10 +37,10 @@ const About = () => {
           </ul>
           <div className="py-4">
             <Button variant="primary">Learn More</Button>
-        article>
-    </section/div>
-      </div>
-    </div>
+          </div>
+        </div>
+      </article>
+    </section>
   );
 };
 
